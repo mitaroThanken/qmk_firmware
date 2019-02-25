21ebc06b436a7b642215bcc15671d1b152aeec44
YAMAMI Taro <mitarothanken@gmail.com>
2019-02-25 12:59:39 +0900

    clang-formatted.

--- keyboards/attack25/rev1/rev1.h
+++ keyboards/attack25/rev1/rev1.h
@@ -2,6 +2,7 @@
 
 #include "quantum.h"
 
+// clang-format off
 #define LAYOUT_ortho_5x5( \
 	K00, K01, K02, K03, K04, \
 	K10, K11, K12, K13, K14, \
@@ -15,3 +16,4 @@
 	{ K30,   K31,   K32,   K33,   K34 }, \
 	{ K40,   K41,   K42,   K43,   K44 }  \
 }
+// clang-format on
