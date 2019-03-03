885c08f7c7f55edd4fd2f6d349115082a688a2d2
YAMAMI Taro <mitarothanken@gmail.com>
2019-03-03 19:42:17 +0900

    [FIX] KC_LGUI/KC_RGUI 押下時、トグル状態をリセット

--- users/mitarothanken/toggle_gui.c
+++ users/mitarothanken/toggle_gui.c
@@ -17,6 +17,12 @@ bool toggle_gui(uint16_t keycode, keyrecord_t *record) {
         }
       }
       break;
+    case KC_LGUI:
+    case KC_RGUI:
+      if (record->event.pressed) {
+        tg_gui = false;
+      }
+      break;
     case KC_ESC:
       if (record->event.pressed) {
         if (tg_gui) {
